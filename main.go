6a7fa0b8d782bbc77f6cda4b84eaf306614228f4
nargok <nargok@gmail.com>
2019-01-12 15:36:11 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+  "os/user"
+)
+
+func main() {
+  user, err := user.Current()
+  if err != nil {
+    panic(err)
+  }
+  fmt.Printf("Hello, %s! This is the Monkey programing language!\n",
+    user.Username)
+  fmt.Printf("Feel free to type in command\n")
+}
+
